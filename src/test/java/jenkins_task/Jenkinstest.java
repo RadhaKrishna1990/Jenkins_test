5d5bddddcf45c960a824cff14288f23b91a32ef6
Radhakrishna Parvatam <radhakrishna_parvatam@epam.com>
2022-01-15 01:11:05 +0530

    my

---
+++ src/test/java/jenkins_task/Jenkinstest.java
@@ -0,0 +1,12 @@
+package jenkins_task;
+
+import org.testng.annotations.Test;
+
+public class Jenkinstest {
+	
+	@Test
+	public void testjenkins() {
+		System.out.println("Print out something");
+	}
+
+}
